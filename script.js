c8730cf848a6456dfec2d83c52b104d679dd5aff
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-25 12:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //your JS code here. If required.
-document.getElementById("my-form").onsubmit = function(e){
+document.getElementById("submit").addEventListener("click", function(e){
     e.preventDefault();
-	
     const firstName = document.getElementById("first-name").value;
     const lastName = document.getElementById("last-name").value;
     const phone = document.getElementById("phone").value;
     const email = document.getElementById("email").value;
-
+    console.log(firstName);
     alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`);
-}
+})
